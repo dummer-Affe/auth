e3fb9b7f6a65370ea246f6f4beb050b2e19cf757
dummer-Affe <85801776+dummer-Affe@users.noreply.github.com>
2022-11-14 03:38:24 +0300

    asdfas

---
+++ lib/loginPages/test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class test extends StatelessWidget {
+  const test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("dfsdf"),);
+  }
+}
